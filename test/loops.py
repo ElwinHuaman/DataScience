acad8df7a0cedc1d7af6f1d3354ad9c6b42fe17d
Elwin Huaman <elwinlhq@hotmail.com>
2018-03-12 18:58:55 +0100

    Second commit

---
+++ test/loops.py
@@ -0,0 +1,21 @@
+# for i in range (0,10):
+#     print(i)
+
+# for j in range (0,20, 3):
+#     print(j)
+
+# for k in range (2, 12, 3):
+#     print(k)
+
+# i = 0
+# while i < 10:
+#     print(i)
+#     i+=1
+
+for i in range(0,10):
+    if i%3 == 0:
+        print(i)
+    elif i%3 == 1:
+        print(10+i)
+    else:
+        print(i-10)
---
+++ test/main.py
@@ -0,0 +1,10 @@
+x = 3
+y = 3.0
+print (x is y)
+print (x == y)
+
+x = "Hello"
+new_x = x.lower()
+new_y = x.capitalize()
+new_z = x.upper()
+print( new_z)
