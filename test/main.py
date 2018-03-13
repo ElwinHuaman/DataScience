89e7bb39882d3b68e8d74c9b22462dbe9d88046e
Elwin Huaman <elwinlhq@hotmail.com>
2018-03-13 18:11:08 +0100

    Second commit Python

--- test/main.py
+++ test/main.py
@@ -1,10 +1,25 @@
-x = 3
-y = 3.0
-print (x is y)
-print (x == y)
+# x = 3
+# y = 3.0
+# print (x is y)
+# print (x == y)
 
-x = "Hello"
-new_x = x.lower()
-new_y = x.capitalize()
-new_z = x.upper()
-print( new_z)
+# x = "Hello"
+# new_x = x.lower()
+# new_y = x.capitalize()
+# new_z = x.upper()
+# print( new_z)
+
+# def print_abs(val):
+#     if val < 0:
+#         print(0-val)
+#     else:
+#         print(val)   
+
+# x = print_abs(-2.7)
+# print(x)
+
+def inc_val(val):
+    val = val+1
+x = 7
+inc_val(x)
+print(x)
